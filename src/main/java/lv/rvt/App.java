c12eef47d2005ba3067642ddc03f8e486eea72f6
23DP3JJAUN <166390119+23DP3JJAUN@users.noreply.github.com>
2024-10-11 06:21:50 +0000

    Message

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,14 +1,21 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        
-        System.out.println("Hellow world");
+        // Mes taisam seit musu uzdevumus   
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+
+        String input = scanner.nextLine();
+
+        System.out.println(input);
 
     }
+    
 
 }
