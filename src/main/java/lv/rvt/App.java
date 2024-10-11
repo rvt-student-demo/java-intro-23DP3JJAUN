986cb79075ee58f62fdce726818b0136fd43e843
23DP3JJAUN <166390119+23DP3JJAUN@users.noreply.github.com>
2024-10-11 06:01:33 +0000

    Uzdevuma nosaukums...

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
         
-        System.out.println("ievadi savu vardu:");
-        String firstname = scanner.nextLine();
-
-        System.out.println("ievadi savu uzvardu:");
-        String lastname = scanner.nextLine();
-
-        System.out.println("ievadi savu grupu:");
-        String group = scanner.nextLine();
-        System.out.println("Sveik "+ firstname + "" + lastname + "" + group);
-        
+        System.out.println("Hellow world");
 
     }
 
