7bacf7506a20289c475389c60e140d63436de64e
23DP3JJAUN <166390119+23DP3JJAUN@users.noreply.github.com>
2024-10-11 05:58:54 +0000

    some task

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,23 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("ievadi savu vardu:");
+        String firstname = scanner.nextLine();
+
+        System.out.println("ievadi savu uzvardu:");
+        String lastname = scanner.nextLine();
+
+        System.out.println("ievadi savu grupu:");
+        String group = scanner.nextLine();
+        System.out.println("Sveik "+ firstname + "" + lastname + "" + group);
+        
 
     }
 
